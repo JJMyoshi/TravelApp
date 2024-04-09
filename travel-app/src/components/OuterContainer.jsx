4bfe380cfa1c7b4d5ac7e76d6861df2c67696bf5
camila-hernandez <114267949+camila-hernandez@users.noreply.github.com>
2024-04-09 17:06:05 -0600

    Removed folders

--- travel-app/src/components/OuterContainer.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "./outerContainer.module.css"
-
-export default function OuterContainer({children}) {
-    return (
-        <div className = "outerContainer">
-            {children}
-        </div>
-    );
-}
--- travel-app/src/components/innerContainer/HeaderTitle.jsx
+++
@@ -1,8 +0,0 @@
-function HeaderTitle( {header_title }){
-
-    return <div>
-        <h3> {header_title} </h3> 
-    </div>
-}
-
-export default HeaderTitle;
